e990ec393d4af2d98acdd478f3232205de6e83d5
Chiu-Ki Chan <chiuki@users.noreply.github.com>
2016-05-14 10:12:46 -0600

    Add MainActivity

---
+++ app/src/main/java/com/sqisland/android/dice/MainActivity.java
@@ -0,0 +1,12 @@
+package com.sqisland.android.dice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
